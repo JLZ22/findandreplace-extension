a6dd818136b9131027133feb9aa553870756edb3
JLZ22 <johnzeng878@gmail.com>
2023-05-13 13:24:43 -0700

    remove prompt function

--- scripts/content.js
+++ scripts/content.js
@@ -8,32 +8,6 @@ function getSelectedText(selection) {
   return text;
 }
 
-function openPrompt(selection) {
-  let phrase = prompt("Enter the word you would like to find in the highlighted text or nothing for total word count.");
-  text = getSelectedText(selection);
-  console.log(phrase);
-  console.log(text);
-  let count = 0;
-  if (!text) {
-    return;
-  }
-  if (!phrase) {
-    count = text.trim().split(/\s+/).length;
-    console.log("count: " + count);
-  }
-  else {
-    const array = text.split(/[.,!,?,;, ]/);
-    console.log(array);
-    for (i = 0 ; i < array.length ; i++) {
-      if (array[i].toUpperCase().includes(phrase.toUpperCase())) {
-        count++;
-      }
-    }
-  }
-  console.log("x");
-  alert(count);
-}
-
 function getHTMLOfSelection () {
   var range;
   if (document.selection && document.selection.createRange) {
