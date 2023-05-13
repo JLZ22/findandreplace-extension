c611ec82f78c9154aab90c6a8760eab65a7bd16f
JLZ22 <johnzeng878@gmail.com>
2023-05-12 21:33:04 -0700

    add web worker to content and transition getSelected function to new file

---
+++ scripts/worker.js
@@ -0,0 +1,9 @@
+function getSelected() {
+  let text = "";
+  if (typeof window.getSelection != "undefined") {
+      text = window.getSelection().toString();
+  } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+      text = document.selection.createRange().text;
+  }
+  return text;
+}
