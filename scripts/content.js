4f8f743b20ec039f15b84f069cfb3d1f95f36210
JLZ22 <johnzeng878@gmail.com>
2023-05-25 20:38:21 -0700

    add function to sanitize html for future replace functionality

--- scripts/content.js
+++ scripts/content.js
@@ -32,6 +32,18 @@ function getHTMLOfSelection () {
   }
 }
 
+/*!
+ * Sanitize and encode all HTML in a user-submitted string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {String} str  The user-submitted string
+ * @return {String} str  The sanitized string
+ */
+var sanitizeHTML = function (str) {
+	var temp = document.createElement('div');
+	temp.textContent = str;
+	return temp.innerHTML;
+};
+
 function find(phrase, selection, html) {
   console.log('phrase: ' + phrase);
   console.log('selection: ' + selection);
