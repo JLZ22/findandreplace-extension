bb890c37f23afe1b24237f73097d9339fcb5f460
JLZ22 <johnzeng878@gmail.com>
2023-07-24 20:29:02 -0700

    restructure how data is sent between popup and content, organize functions in content script, remove function that gets HTML from a selection + the var that held the output of that function, change find() to handleRequest(), complete method to highlight the selection but method to highlight every occurence of phrase in an element is TODO(highlightSelction() is dependent on highlight())

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,26 @@
-console.log("popup running");
+var dict = {phrase: "", status: ""};
+
+/**
+ * Gets string from text box in the popup script,
+ * adds it to a dictionary, and sends it to content script 
+ * every time the text box in the popup is changed.
+ */
 document.getElementById("phrase").addEventListener("input", (ev) => {
   ev.preventDefault();
-  console.log("change")
-  sendData();
+  dict.phrase = document.getElementById("phrase").value;
+  sendData(dict);
 });
 
+/**
+ * Sends the given String to the content script.
+ */
 function sendData() {
-  let phrase = document.getElementById("phrase").value;
-  console.log("phrase: " + phrase)
+  console.log("phrase: " + dict.phrase);
+  console.log("status: " + dict.status);
+  console.log("newPhrase: " + dict.newPhrase);
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     console.log("sending")
-    chrome.tabs.sendMessage(tabs[0].id, phrase);
+    chrome.tabs.sendMessage(tabs[0].id, dict);
     console.log("-----------------------------------------")
   });
 }
--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,4 @@
-function getHTMLOfSelection () {
-  var range;
-  if (document.selection && document.selection.createRange) {
-    range = document.selection.createRange();
-    return range.htmlText;
-  }
-  else if (window.getSelection) {
-    var selection = window.getSelection();
-    if (selection.rangeCount > 0) {
-      range = selection.getRangeAt(0);
-      var clonedSelection = range.cloneContents();
-      var div = document.createElement('div');
-      div.appendChild(clonedSelection);
-      return div.innerHTML;
-    }
-    else {
-      return '';
-    }
-  }
-  else {
-    return '';
-  }
-}
-
-/*!
+/**
  * Sanitize and encode all HTML in a user-submitted string
  * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
  * @param  {String} str  The user-submitted string
@@ -34,40 +10,78 @@ var sanitizeHTML = function (str) {
 	return temp.innerHTML;
 };
 
-function getManipulatedSelection(selection, phrase) {
+/**
+ * Finds and highlights all occurences of a given phrase in
+ * the given selection.
+ * 
+ * @param {String} phrase The phrase to be highlighted.
+ * @param {Selection} selection The selection to be searched.
+ * @param {String} status A string that indicates what request is being made.
+ */
+function handleRequest(phrase, selection, status) {
+  let data = highlightSelection(phrase, selection);
+}
+
+/**
+ * Parses through the common ancestor node which is derived from the 
+ * given selection and highlights all instances of the phrase (case insensitive)
+ * by injecting HTML using innerHTML.  
+ * 
+ * @param {String} phrase The phrase to be highlighted.
+ * @param {Selection} selection The selection to be searched.
+ * @returns A dictionary that has the number of occurences of the phrase
+ *          and the common ancestor Node
+ */
+function highlightSelection(phrase, selection) {
   let common = selection.commonAncestorContainer;
   let count = 0;
   common.childNodes.forEach(element => {
-    let temp = editText(element.textContent, phrase);
-    element.textContent - temp.text;
+    let temp = highlight(phrase, element);
     count += temp.count;
   });
   return {node: common, count: count};
 }
 
-function find(phrase, selection, html) { // broken asf use textContent
-  console.log('phrase: ' + phrase);
-  console.log('selection: ' + selection);
-  // console.log('anchor node: ' + selection.anchorNode);
-  // console.log('focus node: ' + selection.focusNode);
-  // console.log('selection html: ' + html);
-  // let data = editHTML(phrase, html);
-  // console.log('post manipulation html: ' + data.html);
-  let data = getManipulatedSelection(selection, phrase);
+/**
+ * Highlights all instances of text in the node.
+ * @param {String} text The text to be highlighted
+ * @param {Element} el The element to be searched.
+ */
+function highlight(text, el){ // TODO
+  let h = el.innerHTML;
+
+  // let h = el.textContent;
+  // el.textContent = '';
+  // let idx, prev = 0;
+  // while((idx = t.indexOf(text, prev)) !== -1){
+  //   el.append(t.slice(prev, idx));
+  //   const a = document.createElement('a');
+  //   a.href = '/' + text;
+  //   a.textContent = text;
+  //   el.appendChild(a);
+  //   prev = idx + text.length;
+  // }
+  // el.append(t.slice(prev));
 }
 
-function editText(textContent, phrase) {
-  let count = 0;
-  //TODO
-}
+var selection;
 
-var selection, html;
-
+/**
+ * Saves selection to variables every time mouse up.
+ */
 onmouseup = () => {
   selection = window.getSelection();
-  html = getHTMLOfSelection();
 }
 
+
+/**
+ * Listens to message from popup script and calls a handler
+ * to perform actions on the current page according to the 
+ * status from the message.
+ */
 chrome.runtime.onMessage.addListener((message) => {
-  find(message, selection, html);
+  console.log('phrase: ' + message.phrase);
+  console.log('selection: ' + selection);
+  console.log('status: ' + message.status)
+  handleRequest(message.phrase, selection, message.status);
 });
